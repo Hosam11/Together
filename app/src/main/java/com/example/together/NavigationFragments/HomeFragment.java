b7b04db5025a6f7d6cba33a9dea8d9a08f7467d4
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-05-18 19:27:52 +0200

    Update HomeFragment.java

--- app/src/main/java/com/example/together/NavigationFragments/HomeFragment.java
+++ app/src/main/java/com/example/together/NavigationFragments/HomeFragment.java
@@ -8,14 +8,54 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.together.Adapters.HomeRecyclarViewAdapter;
+import com.example.together.Adapters.POJO;
+import com.example.together.BottomNavigationView;
 import com.example.together.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+    HomeRecyclarViewAdapter adapter;
+    ArrayList<POJO> pojos = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+        fab = v.findViewById(R.id.add_group_FAB);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent To Create Group Screen
+            }
+        });
+        recyclerView=v.findViewById(R.id.home_groups_rv);
+        pojos.add(new POJO("Android Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+        pojos.add(new POJO("IOS Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+        pojos.add(new POJO("Kotlin Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+        pojos.add(new POJO("Flutter Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+        pojos.add(new POJO("React Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+        pojos.add(new POJO("Ionic Developing","Hello, my name is mahmoud, I live in KafrEldawar",R.drawable.default_img));
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ((BottomNavigationView)getActivity()).setActionBarTitle("Home");
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(recyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter= new HomeRecyclarViewAdapter(pojos);
+        recyclerView.setAdapter(adapter);
     }
 }
