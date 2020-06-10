626c4ef3a8aef15169c0297022e0f33f1d72747a
mohamed <mohamed.elgharpawi22@icloud.com>
2020-06-11 00:18:04 +0200

    edit To KITKAT

--- app/src/main/java/com/example/together/App.java
+++ app/src/main/java/com/example/together/App.java
@@ -1,6 +1,11 @@
 package com.example.together;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
 import com.pusher.pushnotifications.PushNotifications;
 public class App extends Application {
     @Override
@@ -9,4 +14,10 @@ public class App extends Application {
         PushNotifications.start(getApplicationContext(), "ed3b05e0-b714-487d-a137-8daea3dfdecd");
         PushNotifications.addDeviceInterest("debug-together");
     }
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
+
 }
